3ef83422efda10ef80a88ab12b18393cae616491
Jeann-Rocha <jeannrochasilva@gmail.com>
2024-05-15 23:15:06 -0300

    Modularize as funções de listas duplamente encadeadas

---
+++ src/double_list_functions.h
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef struct Node
+{
+    int iPayload;
+    Node* ptrNext;
+    Node* ptrPrev;
+} Node;
+
+Node* createNode(int);
+void displayList(Node**);
+void insertFront(Node**, int);
+void insertEnd(Node**, int);
+void insertAfter(Node*, int);
+void insertBefore(Node**, Node*, int);
+void deleteNode(Node**, Node*);
+void deleteNodebyValue(Node**, int);
+Node* searchNodebyValue(Node**, int);
+void swapValue(Node*, Node*);
