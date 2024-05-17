ac07104de38ac651bc08f42b7dbbdbcf895d3d23
Yure Santos de Oliveira <yure.sdo@gmail.com>
2024-05-17 10:21:05 -0300

    adicione selection.cpp, time_selection.txt e time_optimized_selection.txt

---
+++ src/selection.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "double_list_functions.h"
+#include "sort_functions.h"
+
+using std::cout;
+using std::endl;
+
+void selectionSort(Node** head)
+{
+    if (*head == nullptr)
+    {
+        cout << "Lista vazia." << endl;
+        return;
+    }
+
+    Node* ptrOuterLoop = *head;
+    Node* ptrInnerLoop = nullptr;
+
+    while (ptrOuterLoop != nullptr)
+    {
+        ptrInnerLoop = ptrOuterLoop->ptrNext;
+
+        while(ptrInnerLoop != nullptr)
+        {
+            if (ptrInnerLoop->iPayload < ptrOuterLoop->iPayload)
+            {
+                swapValue(ptrOuterLoop, ptrInnerLoop);
+            }
+
+            ptrInnerLoop = ptrInnerLoop->ptrNext;
+        }
+
+        ptrOuterLoop = ptrOuterLoop->ptrNext;
+    }
+
+    return;
+}
+
+void optimizedSelectionSort(Node** head)
+{
+    if (*head == nullptr)
+    {
+        cout << "Lista vazia." << endl;
+        return;
+    }
+
+    Node* ptrOuterLoop = *head;
+    Node* ptrInnerLoop = nullptr;
+    Node* ptrMinNode = nullptr;
+
+    while (ptrOuterLoop != nullptr)
+    {
+        ptrMinNode = ptrOuterLoop;
+        ptrInnerLoop = ptrOuterLoop->ptrNext;
+
+        while(ptrInnerLoop != nullptr)
+        {
+            if (ptrInnerLoop->iPayload < ptrMinNode->iPayload)
+            {
+                ptrMinNode = ptrInnerLoop;
+            }
+
+            ptrInnerLoop = ptrInnerLoop->ptrNext;
+        }
+
+        swapValue(ptrOuterLoop, ptrMinNode);
+
+        ptrOuterLoop = ptrOuterLoop->ptrNext;
+    }
+
+    return;
+}
