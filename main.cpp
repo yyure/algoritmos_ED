d75ee9fa08bcbcfde87ebd0c5e51fd5eec256a10
Yure Santos de Oliveira <yure.sdo@gmail.com>
2024-06-09 22:36:17 +0000

    adicione tempo de criação das listas e árvores

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <chrono>
 
@@ -9,6 +10,7 @@
 // iostream
 using std::cout;
 using std::endl;
+using std::ofstream;
 
 // chrono
 using std::chrono::nanoseconds;
@@ -16,7 +18,7 @@ using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 
 int main()
-{   
+{
     /*
     DoubleLinkedList::List<int>* list;
 
@@ -43,11 +45,11 @@ int main()
         free(list);
     }*/
 
-    BinarySearchTree::Node<int>* tree;
+    // BinarySearchTree::Node<int>* tree;
 
     // criar 100 árvores binárias, com 100000 nós cada
     // Analisar o tempo de cada tipo de busca
-    for (int i = 0; i < 100; i++) {
+    /*for (int i = 0; i < 100; i++) {
 
         tree = BinarySearchTree::createTreeNode<int>(1);
 
@@ -63,6 +65,49 @@ int main()
         auto duration = duration_cast<nanoseconds>(end - start);
 
         cout << duration.count() << endl;
+    }*/
+
+
+   
+   // analisar o tempo de criação de listas e árvores
+   int arriNumbers[10000];
+
+    for (int i = 0 ; i < 100; i++)
+    {
+        // preencher o vetor com 10000 números aleatórios
+        for (int j = 0; j < 10000; j++) {
+            arriNumbers[j] = rand() % 100 + 1;
+        }
+
+        // abrir arquivo para armazenar os tempos de criação da lista
+        ofstream time_creation_list("algorithm_times/creation/time_creation_list.txt", std::ios::app);
+
+        // tempo de criação da lista
+        auto start = high_resolution_clock::now();
+        DoubleLinkedList::List<int>* list = DoubleLinkedList::createList<int>();
+        for (int j = 0; j < 10000; j++) {
+            DoubleLinkedList::insertFront(list, arriNumbers[j]);
+        }
+        auto end = high_resolution_clock::now();
+
+        // armazenar o tempo de criação da lista
+        time_creation_list << duration_cast<nanoseconds>(end - start).count() << endl;
+        time_creation_list.close();
+
+        // abrir arquivo para armazenar os tempos de criação da árvore 
+        ofstream time_creation_tree("algorithm_times/creation/time_creation_tree.txt", std::ios::app);
+
+        // tempo de criação da árvore
+        start = high_resolution_clock::now();
+        BinarySearchTree::Node<int>* tree = nullptr;
+        for (int j = 0; j < 10000; j++) {
+            tree = BinarySearchTree::insertNode(tree, arriNumbers[j]);
+        }
+        end = high_resolution_clock::now();
+
+        // armazenar o tempo de criação da árvore
+        time_creation_tree << duration_cast<nanoseconds>(end - start).count() << endl;
+        time_creation_tree.close();
     }
 
     return 0;
